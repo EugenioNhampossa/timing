d74f7cc8c72e0b63c8ee01a17a7201880c92a26b
EugenioNhampossa <eugenioanhampossa@gmail.com>
2023-05-21 09:28:15 +0200

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-linear': '#3D4ABA',
+        pink: '#FD5B71',
+        blue: '#3D4ABA',
+        green: '#07E092',
+        'gray-light': '#FAFAFF',
+        black: '#070417',
+        purple: '#9B51E0',
+        orange: '#FFA656',
+        dark: '#18152C',
+      },
+    },
+    container: {
+      center: true,
+    },
   },
   plugins: [],
-};
-
+}
